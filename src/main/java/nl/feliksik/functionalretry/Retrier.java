a485ba98b5b26d662d2a5514c3329a62951da974
Eric Feliksik <feliksik@gmail.com>
2019-11-17 11:57:46 +0100

    make implementation iterative, which is a bit easier

--- src/main/java/nl/feliksik/functionalretry/Retrier.java
+++ src/main/java/nl/feliksik/functionalretry/Retrier.java
@@ -1,14 +1,9 @@
 package nl.feliksik.functionalretry;
 
-import io.vavr.collection.Stream;
 import io.vavr.control.Either;
 import org.immutables.value.Value;
 
 import java.time.Duration;
-import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public final class Retrier<E, R> {
@@ -24,111 +19,58 @@ public final class Retrier<E, R> {
     }
 
     public final Either<E, R> executeWithRetries(Supplier<Either<E, R>> retriable) {
+        int attemptNr = 0;
+        while (true) {
+            attemptNr++;
+            Either<E, R> result = retriable.get();
+            Attempt<E, R> attempt = Attempt.of(attemptNr, result);
 
-        Consumer<Attempt<E, R>> performUserDefinedSideEffectsAndSleep = attempt -> this.dispatchToHandlerScenario(attempt, getSideEffectHandler());
-        Predicate<Attempt<E, R>> resultMustBeRetried = attempt -> this.dispatchToHandlerScenario(attempt, trueIfAttemptMustBeRetried());
-
-        return Stream.range(1, Integer.MAX_VALUE)
-                .map(attemptNr -> Attempt.of(attemptNr, retriable.get()))
-                .peek(performUserDefinedSideEffectsAndSleep)
-                .dropWhile(resultMustBeRetried)
-                .map(Attempt::result)
-                .head();
-    }
-
-
-    private interface MutuallyExclusiveScenarioHandler<E, R, T> {
-
-        T handleSuccess(Attempt<E, R> a);
-
-        T handleRetriableError(Attempt<E, R> a);
-
-        T handleNonRetriableError(Attempt<E, R> a);
-
-        T handleAttemptsExhausted(Attempt<E, R> a);
-    }
-
-    private MutuallyExclusiveScenarioHandler<E, R, Boolean> trueIfAttemptMustBeRetried() {
-// returns true if a result must be retried, false if it can be returned to the user
-        final Boolean ATTEMPT_MUST_BE_RETRIED = Boolean.TRUE;
-        final Boolean RETURN_TO_USER = Boolean.FALSE;
-        return new MutuallyExclusiveScenarioHandler<E, R, Boolean>() {
-            @Override
-            public Boolean handleSuccess(Attempt a) {
-                return RETURN_TO_USER;
-            }
-
-            @Override
-            public Boolean handleRetriableError(Attempt a) {
-                return ATTEMPT_MUST_BE_RETRIED;
-            }
-
-            @Override
-            public Boolean handleNonRetriableError(Attempt a) {
-                return RETURN_TO_USER;
-            }
-
-            @Override
-            public Boolean handleAttemptsExhausted(Attempt a) {
-                return RETURN_TO_USER;
-            }
-        };
-    }
-
-    private MutuallyExclusiveScenarioHandler<E, R, Void> getSideEffectHandler() {
-        return new MutuallyExclusiveScenarioHandler<E, R, Void>() {
-            @Override
-            public Void handleSuccess(Attempt a) {
-                config.handlers().ifPresent(h -> h.onSuccess(a));
-                return null;
-            }
-
-            @Override
-            public Void handleRetriableError(Attempt a) {
-                config.handlers().ifPresent(h -> h.onRetriableError(a));
-                sleepOrThrowInterrupt(config.backoffFunction().apply(a.attemptNr()));
-                return null;
-            }
-
-            @Override
-            public Void handleNonRetriableError(Attempt a) {
-                config.handlers().ifPresent(h -> h.onNonRetriableError(a));
-                return null;
-            }
-
-            @Override
-            public Void handleAttemptsExhausted(Attempt a) {
-                config.handlers().ifPresent(h -> h.onMaxAttemptsReached(a));
-                return null;
-            }
-
-            private void sleepOrThrowInterrupt(Duration d) {
-                try {
-                    Thread.sleep(d.toMillis());
-                } catch (InterruptedException interrupted) {
-                    Thread.currentThread().interrupt();
-                    throw new RuntimeException("Interrupted during retry backoff", interrupted);
+            if (attempt.isSuccess()) {
+                notifySuccess(attempt);
+                return result;
+            } else {
+                if (failureIsNotRetriable(result.getLeft())) {
+                    notifyNonRetriableError(attempt);
+                    return result;
+                } else if (attemptNr >= config.maxAttempts()) {
+                    notifyAttemptsExhausted(attempt);
+                    return result;
+                } else {
+                    notifyRetriableError(attempt);
+                    sleepOrThrowInterrupt(config.backoffFunction().apply(attempt.attemptNr()));
                 }
             }
-        };
+        }
     }
 
-    private <T> T dispatchToHandlerScenario(Attempt<E, R> attempt, MutuallyExclusiveScenarioHandler<E, R, T> handler) {
-        if (attempt.isSuccess()) {
-            return handler.handleSuccess(attempt);
-        } else if (failureIsNotRetriable(attempt.result().getLeft())) {
-            return handler.handleNonRetriableError(attempt);
-        } else if (attempt.attemptNr() >= this.config.maxAttempts()) {
-            return handler.handleAttemptsExhausted(attempt);
-        } else {
-            return handler.handleRetriableError(attempt);
+    public void notifySuccess(Attempt a) {
+        config.handlers().ifPresent(h -> h.onSuccess(a));
+    }
+
+    public void notifyRetriableError(Attempt a) {
+        config.handlers().ifPresent(h -> h.onRetriableError(a));
+    }
+
+    public void notifyNonRetriableError(Attempt a) {
+        config.handlers().ifPresent(h -> h.onNonRetriableError(a));
+    }
+
+    public void notifyAttemptsExhausted(Attempt a) {
+        config.handlers().ifPresent(h -> h.onMaxAttemptsReached(a));
+    }
+
+    private void sleepOrThrowInterrupt(Duration d) {
+        try {
+            Thread.sleep(d.toMillis());
+        } catch (InterruptedException interrupted) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Interrupted during retry backoff", interrupted);
         }
     }
 
     private boolean failureIsNotRetriable(E failure) {
         return !this.config.retryFailureIf().test(failure);
     }
-
 
     @Value.Immutable
     @ImmutableStyles.Entity
